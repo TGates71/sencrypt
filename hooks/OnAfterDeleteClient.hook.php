d0abc8cf05a526954ed3d23483a4f4dd2391658c
Duke City Solutions, LLC <anthony@dcsnm.com>
2022-12-09 14:23:31 -0700

    Sencrypt 2.0.0 Updates/fixes/security fixes
    Sencrypt 2.0.0 Updates/fixes/security fixes.

--- hooks/OnAfterDeleteClient.hook.php
+++ hooks/OnAfterDeleteClient.hook.php
@@ -1,9 +1,12 @@
 <?php
 /**
- * Delete/revoke after user deleted script for Sencrypt Module for Sentora 1.0.3
- * Version : 002
- * Author : TGates
- * Additional work by Diablo925
- */
+	* Controller for sencrypt module for sentora version 1.0.3
+	* Version : 100
+	* Author  : Jettaman, Sentora Team, Diablo925, TGages
+	* Script credits  : Diablo925, TGages
+*/
+
 // remove client's certificates and files if any
+
+
 ?>
