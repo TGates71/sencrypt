1db17a72454963aed1a4b25e8cc4c8908b313fb9
TGates <tgates@mach-hosting.com>
2023-04-11 11:40:30 -0400

    Removed unused files

--- hooks/OnAfterDeleteClient.hook.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Delete/revoke after user deleted script for Sencrypt Module for Sentora 1.0.3
- * Version : 002
- * Author : TGates
- * Additional work by Diablo925
- */
-// remove client's certificates and files if any
-?>
